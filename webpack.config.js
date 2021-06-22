ae631e98f8e2a7aed51999108980a523a5974ee5
Varun Garg <varun.garg@e.lloydsbanking.com>
2021-06-22 14:10:46 +0530

    changed build folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     index: './src/index.js',
   },
  output: {
-   path: path.join(__dirname, '/dist'),
+   path: path.join(__dirname, '/build'),
    filename: 'index.bundle.js',
  },
  devServer: {
